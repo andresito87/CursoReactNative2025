4a7d30ddd3caa7480252aa41bcd889ca2f4284ec
Andres <andrespodadera87@gmail.com>
2025-11-20 09:07:19 +0100

    Finished Section 6 - Flexbox

--- 02-MyFirstApp/App.tsx
+++ 02-MyFirstApp/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
-// import { CounterScreen } from './src/presentation/screens/CounterScreen'
-// import { HelloWorldScreen } from './src/presentation/screens/HelloWorldScreen'
-import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen'
 import { PaperProvider } from 'react-native-paper'
 import IonIcon from '@react-native-vector-icons/ionicons';
+// import { CounterScreen } from './src/presentation/screens/CounterScreen'
+// import { HelloWorldScreen } from './src/presentation/screens/HelloWorldScreen'
+// import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen'
+// import { BoxObjectModelScreen } from './src/presentation/screens/BoxObjectModelScreen';
+// import { DimensionScreen } from './src/presentation/screens/DimensionScreen';
+// import { PositionScreen } from './src/presentation/screens/PositionScreen';
+// import { FlexScreen } from './src/presentation/screens/FlexScreen';
+// import { FlexDirectionScreen } from './src/presentation/screens/FlexDirectionScreen';
+import { HomeworkScreen } from './src/presentation/screens/HomeworkScreen';
 
 export const App = () => {
   return (
@@ -14,7 +20,13 @@ export const App = () => {
       <SafeAreaView style={{ flex: 1 }}>
         {/* <HelloWorldScreen name='Andrés Podadera' /> */}
         {/* <CounterScreen /> */}
-        <CounterM3Screen />
+        {/* <CounterM3Screen /> */}
+        {/* <BoxObjectModelScreen /> */}
+        {/* <DimensionScreen /> */}
+        {/* <PositionScreen /> */}
+        {/* <FlexScreen /> */}
+        {/* <FlexDirectionScreen /> */}
+        <HomeworkScreen />
       </SafeAreaView>
     </PaperProvider>
   )
