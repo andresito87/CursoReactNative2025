b6e49bad76142e62820e8af0a1a662e6e45b6542
Andres <andrespodadera87@gmail.com>
2025-11-25 10:44:15 +0100

    Completed Carousel Movies with ScrollView

--- 06-MoviesApp/src/presentation/screens/home/HomeScreen.tsx
+++ 06-MoviesApp/src/presentation/screens/home/HomeScreen.tsx
@@ -1,15 +1,26 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { ScrollView, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useMovies } from '../../hooks/useMovies';
+import { PosterCarousel } from '../../components/movies/PosterCarousel';
 
 export const HomeScreen = () => {
 
-    const { } = useMovies()
+    const { isLoading, nowPlaying } = useMovies()
+
+    if (isLoading) {
+        return (<Text>Cargando ...</Text>)
+    }
 
     return (
-        <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right']}>
-            <Text>Home Screen</Text>
-        </SafeAreaView>
+        <ScrollView>
+            <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right']}>
+                <View style={{ marginTop: 20, paddingBottom: 30 }}>
+                    <PosterCarousel
+                        movies={nowPlaying}
+                    />
+                </View>
+            </SafeAreaView>
+        </ScrollView>
     );
 };
