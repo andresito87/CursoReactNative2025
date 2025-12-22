947c98502911086128f4743f72cdca8e2ff731bf
andrespodadera <andrespodadera87@gmail.com>
2025-12-22 18:56:04 +0100

    Init Section 14 - Pokedex

---
+++ 08-pokedex/src/presentation/navigator/StackNavigator.tsx
@@ -0,0 +1,23 @@
+
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { HomeScreen } from "../screens/home/HomeScreen";
+import { PokemonScreen } from "../screens/pokemon/PokemonScreen";
+import { SearchScreen } from "../screens/search/SearchScreen";
+
+export type RootStackParams = {
+    HomeScreen: undefined,
+    PokemonScreen: { pokemonId: number; },
+    SearchScreen: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParams>();
+
+export const StackNavigator = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="PokemonScreen" component={PokemonScreen} />
+            <Stack.Screen name="SearchScreen" component={SearchScreen} />
+        </Stack.Navigator>
+    );
+};
